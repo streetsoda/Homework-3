add2a46eaedd7ad1f67d846c02144ebcaa0fd3f9
streetsoda <34893003+streetsoda@users.noreply.github.com>
2019-02-03 16:21:08 +0200

    Add files via upload

---
+++ sumbybills.php
@@ -0,0 +1,21 @@
+﻿<?php
+function billNumber(int $amount)
+{
+    $denominations = array (500, 200, 100, 50, 20, 10, 5, 2, 1);
+    foreach ($denominations as $denomination) {
+        if ($amount >= $denomination) {
+            $numOfBills = (int)($amount / $denomination);
+            if ($numOfBills > 0) {
+                $amount %= $denomination;
+                echo "$denomination: $numOfBills\n";
+            }
+        }
+    }
+}
+
+    $amount = (int)$argv[1];
+    if ($amount >= 1 && $amount <= 100000) {
+        billNumber($amount);
+    } else {
+        echo "Please enter amount from 1 to 100000";
+    }
