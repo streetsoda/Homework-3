c82e34446f09cf666ba79dac5ec1d8f3f8c8be93
streetsoda <34893003+streetsoda@users.noreply.github.com>
2019-02-03 17:09:45 +0200

    Update sumbybills.php

--- sumbybills.php
+++ sumbybills.php
@@ -1,11 +1,16 @@
-﻿<?php
+<?php
+//php sumbybills.php *enter your sum here*
+
 function billNumber(int $amount)
 {
     $denominations = array (500, 200, 100, 50, 20, 10, 5, 2, 1);
-    foreach ($denominations as $denomination) {
-        if ($amount >= $denomination) {
+    foreach ($denominations as $denomination) 
+    {
+        if ($amount >= $denomination) 
+        {
             $numOfBills = (int)($amount / $denomination);
-            if ($numOfBills > 0) {
+            if ($numOfBills > 0) 
+            {
                 $amount %= $denomination;
                 echo "$denomination: $numOfBills\n";
             }
@@ -14,8 +19,11 @@ function billNumber(int $amount)
 }
 
     $amount = (int)$argv[1];
-    if ($amount >= 1 && $amount <= 100000) {
+    if ($amount >= 1 && $amount <= 100000) 
+    {
         billNumber($amount);
-    } else {
+    } 
+    else 
+    {
         echo "Please enter amount from 1 to 100000";
     }
